d56ec77b4bcb4151ab9492fe03fafad2043ead97
jasondamico <damicoj@union.edu>
2020-12-22 19:06:34 -0500

    Added ability to exit queue adding
    Via a "-quit" flag, which is picked up by an independent method that processes query inputs

--- src/queue_bot.py
+++ src/queue_bot.py
@@ -24,9 +24,22 @@ class QueueBot():
 
         while self.active:
             query = input("Enter the name of the track you would like to add to your queue: ")
+            query = self.__process_queue_query(query)
             
-            tracks = self.spotify.search(query, search_type="track")
-            track_uri = self.spotify.get_first_track_uri(tracks)
+            if self.active:
+                tracks = self.spotify.search(query, search_type="track")
+                track_uri = self.spotify.get_first_track_uri(tracks)
 
-            self.spotify.add_to_queue(track_uri)
+                self.spotify.add_to_queue(track_uri)
 
+    def __process_queue_query(self, query):
+        """
+        Performs all actions necessary based on the passed add-to-queue query and returns an altered query when appropriate flags are entered.
+
+        :param query: The keyword query entered by the user.
+        :return: Either the same query passed to the method or a modified query based on the contents of the arguement.
+        """
+        if query == "-quit":
+            self.active = False
+        else:
+            return query
